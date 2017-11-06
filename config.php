e4dacea5974de080bcd7a854e2f92b0f2dcb6ba0
Aniruddha Jere <ecanarys8@outlook.com>
2017-11-06 14:54:08 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'DevOps For PHP ',
+        'nav_menu' => [
+            '' => 'Microsoft',
+            /*'about-us' => 'About Us',
+            'products' => 'Products',
+            'contact' => 'Contact',*/
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'pretty_uri' => true,
+        'version' => 'v2.0',
+    ];
+
+    return isset($config[$key]) ? $config[$key] : null;
+}
